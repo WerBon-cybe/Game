bfaef8ab43389407562430cb2c587ee4fef64e91
PabloEscobar213 <pavel.pisunovich@mail.ru>
2022-12-03 19:46:32 +0300

    GameEdition

--- Game/Program.cs
+++ Game/Program.cs
@@ -4,45 +4,61 @@ using System.Globalization;
 
 class players
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        ///////////////////////////////////////////первый игрок////////////////////////////////////////////////
-        Console.WriteLine("Введите имя игрока 1: ");
-        string pers = Console.ReadLine();
+        ///////////////////////////////////////////первый игрок//////////////////////////////////////////////////////////////////////////////////
+        string pers = "Alex";
         int agility = 0;
         int power = 0;
         int stamina = 0;
-        ///////////////////////////////////////////первый игрок////////////////////////////////////////////////
-        Console.WriteLine("Введите имя игрока 2: ");
-        string pers1 = Console.ReadLine();
+        ///////////////////////////////////////////первый игрок//////////////////////////////////////////////////////////////////////////////////
+        string pers1 = "Bob";
         int agility1 = 0;
         int power1 = 0;
         int stamina1 = 0;
-
         try
         {
             Console.WriteLine("Введите характеристики персонажей, но учитывайте, что сумма всех характеристик не должна превышать 8! ");
             /////////////////////////////////////////первый игрок////////////////////////////////////////////////////////
-            Console.WriteLine($"Введите ловкость {pers}");
+            Console.WriteLine("Введите ловкость Alex: ");
             agility = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Введите силу {pers}");
+            Console.WriteLine("Введите силу Alex: ");
             power = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Введите выносливость {pers}");
+            Console.WriteLine("Введите выносливость Alex: ");
             stamina = Convert.ToInt32(Console.ReadLine());
             ///////////////////////////////////////Второй игрок///////////////////////////////////////////////////////////////
-            Console.WriteLine($"Введите ловкость {pers1}");
+            Console.WriteLine("Введите ловкость Bob: ");
             agility1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Введите силу {pers1}");
+            Console.WriteLine("Введите силу Bob: ");
             power1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Введите выносливость {pers1}");
+            Console.WriteLine("Введите выносливость Bob: ");
             stamina1 = Convert.ToInt32(Console.ReadLine());
         }
         catch
         {
             Console.WriteLine("мимо вопрос");
         }
-
-        /////////////// 1 бой ///////////////////////////
+        ////////////////////////////Общая сумма//////////////////////////////////////////
+        int obshak = agility + power + stamina;
+        int obshak1 = agility1 + power1 + stamina1;
+        ////////////////////////////проверка, чтобы не меньше двух////////////////////////////////////////////////
+        if (agility <= 1 || stamina <= 1 || power <= 1 || agility1 <= 1 || stamina1 <= 1 || power1 <= 1)
+        {
+            Console.WriteLine("Я же просил не меньше двух!!!!");
+            players.Main();
+        }
+        ////////////////////////////Проверка не больше восьми//////////////////////////////////////////
+        if (obshak > 8 || obshak1 > 8)
+        {
+            Console.WriteLine("Я же сказал не больше 8!!");
+            players.Main();
+        }
+        ////////////////////////////проверка не меньше восьми//////////////////////////////////////////
+        if (obshak < 8 || obshak1 < 8)
+        {
+            Console.WriteLine("Я же сказал чтобы было  8!!");
+            players.Main();
+        }
         string precessing = "0"; // Будут задаваться значения боя;
         /* 
          0 - Нет действий
@@ -63,22 +79,22 @@ class players
         if (agility == agility1)
         {
             monetka = rnd.Next(1, 2);
-                if(monetka == 1)
-                {
-                    precessing = "1";
-                     healpoint1 = healpoint1 - 2;
-                    Console.WriteLine($"HealPoint's {pers1} = {healpoint1}");
-                    stamina1 = stamina1 - 1;
-                }
-                else
-                {
-                    precessing = "1";
-                    healpoint = healpoint - 2;
-                    Console.WriteLine($"HealPoint's {pers} = {healpoint}");
-                    stamina = stamina - 1;
-                }
+            if (monetka == 1)
+            {
+                precessing = "1";
+                healpoint1 = healpoint1 - 2;
+                Console.WriteLine($"HealPoint's {pers1} = {healpoint1}");
+                stamina1 = stamina1 - 1;
+            }
+            else
+            {
+                precessing = "1";
+                healpoint = healpoint - 2;
+                Console.WriteLine($"HealPoint's {pers} = {healpoint}");
+                stamina = stamina - 1;
+            }
         }
-        if(agility >= agility1)
+        if (agility >= agility1)
         {
             precessing = "1";
             healpoint1 = healpoint1 - 2;
@@ -100,7 +116,8 @@ class players
                 break;
 
             case "1":
-                if(stamina < stamina1 ) {
+                if (stamina < stamina1)
+                {
                     Console.WriteLine($"В первом бою победил {pers}");
                     Console.WriteLine($"{pers} нанёс удар в полную силу");
                     Console.WriteLine("----------------------------------------------------------------------");
@@ -145,3 +162,5 @@ class battle3
 
     }
 }
+
+
